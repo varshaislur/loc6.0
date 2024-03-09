c828077e0dc97f038138714213b0b015cfb9a6ca
Sharans23 <sharan.shetty2309@gmail.com>
2024-03-09 16:16:30 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navitems from "./Components/Navitems";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello ho</h1>
+      <Navitems/>
+      <Outlet />
     </div>
   );
 }
